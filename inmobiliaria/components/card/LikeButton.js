2e939ed5e0a090200154f615b23ec40e8b471115
carloseag1609 <carloseag1233@gmail.com>
2021-06-15 18:41:40 -0500

    Change LikeButton component
    Changed View parent component to TouchableOpacity component for a better press feedback, and icon switch using a state.

--- inmobiliaria/components/card/LikeButton.js
+++ inmobiliaria/components/card/LikeButton.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function LikeButton() {
+  const [liked, setLiked] = useState(false);
   return (
-    <View style={{
-      backgroundColor: '#00B074',
-      padding: 7,
-      borderRadius: 100,
-      position: "absolute",
-      right: -8,
-      top: 0
-    }}>
-      <Icon name="favorite" color="#FFF" size={15}/>
-    </View>
+    <TouchableOpacity
+        style={styles.likeButton}
+        onPress={() => setLiked(!liked)}
+        activeOpacity={0.5}
+    >
+      <Icon
+          name={liked ? "favorite" : "favorite-border"}
+          color="#FFF"
+          size={15}
+      />
+    </TouchableOpacity>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  likeButton: {
+    backgroundColor: '#00B074',
+    padding: 7,
+    borderRadius: 100,
+    position: "absolute",
+    right: -8,
+    top: 3
+  }
+})
